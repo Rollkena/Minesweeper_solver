c5442298e718761badfdf17063b995bb2d93e981
Rollkena <rollkena@gmail.com>
2022-11-09 20:59:22 +0500

    запуск браузера + выбор сложности

--- Saper_1/Program.cs
+++ Saper_1/Program.cs
@@ -1,30 +1,94 @@
 ﻿using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Saper_1
 {
     class Program
     {
+        public static int n;
+        public static int m;
         static void Main(string[] args)
         {
             IWebDriver driver;
+            string us_answ;
+            int num_ans;
 
-
-            Console.WriteLine("Введите уровень сапера:\n1 - (beginner)\n2 - (Intermediate)\n3 - Expert");
-            Console.ReadLine();
+            Console.WriteLine("Введите уровень сапера:\n1 - (beginner)\n2 - (intermediate)\n3 - (expert)");
+            while (!((us_answ = Console.ReadLine()) != null && int.TryParse(us_answ, out num_ans) == true && (num_ans >= 1 && num_ans <= 3))) 
+            {
+                Console.WriteLine("Проверьте корректность ввода.");
+            }
+            //выбор сложности
 
             try
             {
-                driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
-
-                driver.Navigate().GoToUrl("https://www.chess.com/login_and_go?returnUrl=https://www.chess.com/");
-
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.BinaryLocation = "D:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+                driver = new OpenQA.Selenium.Chrome.ChromeDriver(chromeOptions);
+                driver.Navigate().GoToUrl("https://minesweeper.online");
                 driver.Manage().Window.Maximize();
+
+                switch (us_answ)
+                {
+                    case "1":
+                        driver.FindElement(By.XPath("//*[@id=\"homepage\"]/div[1]/div[1]/a")).Click();
+                        n = 9;
+                        m = 9;
+                        break;
+
+                    case "2":
+                        driver.FindElement(By.XPath("//*[@id=\"homepage\"]/div[1]/div[2]/a")).Click();
+                        n = 16;
+                        m = 16;
+                        break;
+
+                    case "3":
+                        driver.FindElement(By.XPath("//*[@id=\"homepage\"]/div[1]/div[3]/a")).Click();
+                        n = 30;
+                        m = 16;
+                        break;
+                }
+                IWebElement elem;
+
+                int[,] matri = new int[n, m];
+                for (int i = 0; i <= n; i++)
+                {
+                    for(int j = 0; j <= m; j++)
+                    {
+                        elem = driver.FindElement(By.Id($"cell_{n - 1}_{m - 1}"));
+                        string status = elem.GetAttribute("class");
+                        if (status.Contains("closed"))
+                        {
+                            matri[n,m] = -1;
+                        }
+                        else if (status.Contains("opened"))
+                        {
+                            matri[n,m] = (int)status[status.Length - 1];
+                        }
+                    }
+                }
+                for (int i = 0; i <= (n * m); i++)
+                {
+                    for (int j = 0; j <= m; j++)
+                    {
+                        Console.WriteLine(matri[n, m]);
+                    }
+                }
+
+
+
+
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("Что-то пошло не так\n( • ᴖ • ｡) ");
+                //Console.WriteLine($"Что-то не так\n( • ᴖ • ｡)");
+                Console.WriteLine($"Что-то не так\n( • ᴖ • ｡) {ex.ToString()}");
             }
+            //открытие браузера + выбор сложности
+
+            
         }
     }
+
 }
