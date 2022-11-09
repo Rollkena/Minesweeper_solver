3f303ea9a116fc492eb3b710d9afc1f92f553c8d
Rollkena <rollkena@gmail.com>
2022-11-09 17:20:18 +0500

    Добавьте файлы проекта.

---
+++ Saper_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Saper_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+
+
+            Console.WriteLine("Введите уровень сапера:\n1 - (beginner)\n2 - (Intermediate)\n3 - Expert");
+            Console.ReadLine();
+
+            try
+            {
+                driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+
+                driver.Navigate().GoToUrl("https://www.chess.com/login_and_go?returnUrl=https://www.chess.com/");
+
+                driver.Manage().Window.Maximize();
+            }
+            catch
+            {
+                Console.WriteLine("Что-то пошло не так\n( • ᴖ • ｡) ");
+            }
+        }
+    }
+}
